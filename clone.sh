914a066f0b3a20e041ca00dd7f6c7ff430fde11d
Raisan <yuri@rainyan.net>
2023-01-25 22:09:55 +0900

    Create clone.sh

---
+++ clone.sh
@@ -0,0 +1,8 @@
+# API SERVER
+git clone https://github.com/frea-search/api-server.git
+cd api-server
+git submodule update --init --recursive
+
+# UI SERVER
+git clone https://github.com/Ablaze-MIRAI/FreaSearch-www.git
+mv FreaSearch-www frea-ui
