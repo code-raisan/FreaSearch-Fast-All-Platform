e1415970c37ce7b9efbeb937d6fc62061125ef06
Raisan <yuri@rainyan.net>
2023-01-25 22:28:54 +0900

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -4,6 +4,7 @@ cd api-server
 git submodule update --init --recursive
 
 # UI SERVER
+cd ..
 git clone https://github.com/Ablaze-MIRAI/FreaSearch-www.git
 mv FreaSearch-www frea-ui
 mv Dockerfile ./frea-ui/
